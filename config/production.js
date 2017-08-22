007ab58b236df0ef3ec30da44f8b1b41e9e84c2d
Tucker Buchy <tucker.buchy@gmail.com>
2017-08-22 16:43:52 -0700

    allowed production.js

---
+++ config/production.js
@@ -0,0 +1,10 @@
+// TODO: Actually probably want to remove this. Lets do a copy during the build.
+
+module.exports = {
+    api: {
+        protocol: 'https',
+        hostname: 'pathways-backend.herokuapp.com',
+        port: 443,
+        auth: ''
+    }
+};
