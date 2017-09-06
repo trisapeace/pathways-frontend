f0f760b17a754dbcb44d505f3153c42db67727c1
Dylan McCall <dylan@affinitybridge.com>
2017-09-05 17:28:08 -0700

    Show a filters form based on the search template
    This form is rendered using react-jsonschema-form. By default it renders
    plain HTML form elements with Bootstrap classnames. We will need to
    replace its widgets with our own which use material-ui.

--- src/js/components/ServiceFiltersDialog.js
+++ src/js/components/ServiceFiltersDialog.js
@@ -2,23 +2,32 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
+import {inject, observer} from 'mobx-react';
+
+import Form from "react-jsonschema-form";
+
 import Button from 'material-ui/Button';
 import Dialog, {DialogActions, DialogContent, DialogTitle} from 'material-ui/Dialog';
 import Typography from 'material-ui/Typography';
 
+@inject('apiStore')
+@observer
 export default class ServiceFiltersDialog extends React.Component {
     static propTypes = {
+        apiStore: PropTypes.object.isRequired,
         onRequestClose: PropTypes.func
     };
 
     render() {
+        const {apiStore} = this.props;
+
         return (
             <Dialog maxWidth="md" {...this.props}>
                 <DialogTitle>Search</DialogTitle>
                 <DialogContent>
-                    <Typography type="caption" component="p">
-                        Filter form goes here.
-                    </Typography>
+                    <Form schema={apiStore.searchSchema} onSubmit={this._onFormSubmit.bind(this)}>
+                        <button ref={(elem) => {this._submitButton = elem}} hidden={true} />
+                    </Form>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={this._onCancelClick.bind(this)} color="primary">
@@ -32,15 +41,20 @@ export default class ServiceFiltersDialog extends React.Component {
         );
     }
 
-    _onRequestClose() {
-        this.props.onRequestClose();
-    }
-
-    _onCancelClick() {
-        this.props.onRequestClose();
+    _onFormSubmit({formData}) {
+        console.log("Form submitted", formData);
+        this._onRequestClose();
     }
 
     _onOkClick() {
+        this._submitButton.click();
+    }
+
+    _onCancelClick() {
+        this._onRequestClose();
+    }
+
+    _onRequestClose() {
         this.props.onRequestClose();
     }
 }
