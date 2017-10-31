024db677739793a7ce15f0eb7cf2b2991dc0fd6a
Dylan McCall <dylan@dylanmccall.com>
2017-10-31 13:24:32 -0700

    Additional PathwayDetailView features
     - Show ServiceMapView in a dialog when action buttons are clicked.
     - Dim actions when completed.
     - Visual enhancements and improved ripple effect around checkboxes.
     - Only expand one action at a time.
    
    Issue #11

--- src/js/views/PathwayDetailView.js
+++ src/js/views/PathwayDetailView.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
+import ServiceMapView from 'views/ServiceMapView';
+
+import AppViewContainer_Dialog from 'ui-components/AppViewContainer_Dialog';
 import SimpleAppView from 'ui-components/SimpleAppView';
 
 import {PaperCard} from 'polymer/paper-card';
-import {PaperListbox} from 'polymer/paper-listbox';
-import {PaperItem} from 'polymer/paper-item';
 import {PaperIconButton} from 'polymer/paper-icon-button';
 import {PaperCheckbox} from 'polymer/paper-checkbox';
 import {IronCollapse} from 'polymer/iron-collapse';
@@ -21,7 +22,7 @@ class PathwayDetailView_Main extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selected: undefined
+            selectedActionId: undefined
         };
     }
 
@@ -35,33 +36,48 @@ class PathwayDetailView_Main extends React.Component {
             <div>
                 <PaperCard className="full-card">
                     <div className="card-content">
-                        <p>This is a single pathway: <em>{params.pathwayId}</em>.</p>
+                        <p>Short description of this pathway, <em>{params.pathwayId}</em>.</p>
                     </div>
                 </PaperCard>
                 {
                     actions.map(
-                        (label, index) => <PathwayActionCard key={index} />
+                        (actionId, index) => (
+                            <PathwayActionCard
+                                key={index}
+                                actionId={actionId}
+                                opened={this.state.selectedActionId === actionId}
+                                onSelect={this._onActionSelect.bind(this)}
+                            />
+                        )
                     )
                 }
             </div>
         );
     }
+
+    _onActionSelect(actionId) {
+        this.setState({selectedActionId: actionId});
+    }
 }
 
-class PathwayActionCard extends React.PureComponent {
+export class PathwayActionCard extends React.PureComponent {
     static propTypes = {
-        opened: PropTypes.bool
+        actionId: PropTypes.number.isRequired,
+        opened: PropTypes.bool.isRequired,
+        onSelect: PropTypes.func.isRequired
     };
 
     constructor(props) {
         super(props);
         this.state = {
-            opened: false
+            completed: false,
+            showDialog: false
         };
     }
 
     render() {
-        const {opened} = this.state;
+        const {opened} = this.props;
+        const {completed, showDialog} = this.state;
 
         const label = "Pathway action";
 
@@ -69,38 +85,68 @@ class PathwayActionCard extends React.PureComponent {
 
         const classes = ['pathway-action-item'];
         if (opened) classes.push('opened');
+        if (completed) classes.push('completed');
+
+        const searchDialog = (
+            <AppViewContainer_Dialog
+                appView={<ServiceMapView initialSearch={{what: 'EMPLOYMENT'}} />}
+                isOpen={showDialog}
+                onRequestClose={this._onDialogRequestClose.bind(this)}
+            />
+        );
 
         return (
             <PaperCard className={classes.join(' ')} elevation={(opened) ? 2 : 1}>
-                <div className="card-content interactive" onClick={this._onMainClick.bind(this)}>
+                <div className="card-content">
                     <div className="pathway-action-item--main">
                         <div className="pathway-action-item--checkbox">
-                            <PaperCheckbox onClick={this._onCheckboxClick.bind(this)} noink={true} />
+                            <PaperCheckbox checked={completed} onChange={this._onCheckboxChange.bind(this)} />
                         </div>
-                        <div className="pathway-action-item--header">
+                        <div className="pathway-action-item--header interactive" onClick={this._onMainClick.bind(this)}>
                             <span className="pathway-action-item--label">{label}</span>
                             <IronIcon className="pathway-action-item--toggle" icon={toggleIcon} />
+                            <PaperRipple />
                         </div>
                     </div>
-                    <PaperRipple />
                 </div>
                 <IronCollapse opened={opened} className="card-actions">
                     <div className="pathway-action-item--actions">
-                        <PaperIconButton icon="icons:search" />
-                        <PaperIconButton icon="icons:list" />
-                        <PaperIconButton icon="icons:alarm" />
+                        <PaperIconButton icon="icons:search" onClick={this._onMapSearchClick.bind(this)} />
+                        <PaperIconButton icon="icons:list" onClick={this._onListSearchClick.bind(this)} />
+                        <PaperIconButton icon="icons:alarm" onClick={this._onAlarmClick.bind(this)} />
                     </div>
                 </IronCollapse>
+                {searchDialog}
             </PaperCard>
         );
     }
 
-    _onMainClick(e) {
-        e.preventDefault();
-        this.setState({opened: !this.state.opened});
+    _onMainClick() {
+        const {actionId, opened, onSelect} = this.props;
+        if (onSelect) onSelect(opened ? undefined : actionId);
     }
-    _onCheckboxClick(e) {
-        e.stopPropagation();
+
+    _onCheckboxChange() {
+        this.setState({completed: !this.state.completed});
+    }
+
+    _onMapSearchClick(e) {
+        e.preventDefault();
+        this.setState({showDialog: true});
+    }
+
+    _onListSearchClick(e) {
+        e.preventDefault();
+        this.setState({showDialog: true});
+    }
+
+    _onAlarmClick(e) {
+        e.preventDefault();
+        alert("🤖 Beep. Beep. Beep. 🤖");
+    }
+
+    _onDialogRequestClose() {
+        this.setState({showDialog: false});
     }
 }
 
