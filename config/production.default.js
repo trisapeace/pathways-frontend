b7d6249ed594345d75a6faa23a1ce03f437d5825
rasmus <rasmus@peacegeeks.org>
2018-03-14 14:15:17 -0700

    Issue #26 Replace existing client with a react native app

--- config/production.default.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    api: {
-        protocol: 'https',
-        hostname: 'pathways-backend.herokuapp.com',
-        port: 443,
-        auth: ''
-    }
-};
--- src/js/polymer/paper-badge.js
+++
@@ -1,9 +0,0 @@
-require('paper-badge/paper-badge.html');
-
-import PolymerComponent from 'polymer/util/PolymerComponent';
-
-export class PaperBadge extends PolymerComponent {
-    static options = {
-        element: "paper-badge"
-    };
-}
