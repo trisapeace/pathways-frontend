a1b597313069d9f7c6e579544de2b6ef27fd6b15
rasmus <rasmus@peacegeeks.org>
2018-03-14 14:31:24 -0700

    Issue #26 Create react application

--- App.js
+++ App.js
@@ -1,23 +1,80 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Alert, View, Text, TextInput, Button } from 'react-native';
 
-export default class App extends React.Component {
+class Greeting extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Text>Hello {this.props.name}!</Text>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isShowingText: true };
+
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+class MyButton extends Component {
+  render() {
+    return (
+      <Button onPress={this.props.onClick} title={this.props.title} />
+    );
+  }
+}
+
+class Output extends Component {
+  render() {
+    return (
+      <Text>The value is {this.props.value}</Text>
+    );
+  }
+}
+
+class TwiceTheOutput extends Component {
+  render() {
+    return (
+      <Text>The value is {2 * this.props.value}</Text>
+    );
+  }
+}
+
+export default class HelloWorldApp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: 0,
+    };
+  }
+  render() {
+    return (
+      <View style={{ alignItems: 'center' }}>
+        <Greeting name='Valeera' />
+        <Blink text='I love to blink' />
+        <View style={{
+          flexDirection: 'row',
+          padding: 20,
+        }}>
+          <MyButton title='Make it four' onClick={() => this.setState({ value: 4 })} />
+          <MyButton title='Make it two' onClick={() => this.setState({ value: 2 })} />
+        </View>
+        <Output value={this.state.value} />
+        <TwiceTheOutput value={this.state.value} />
+      </View>
+    );
+  }
+}
