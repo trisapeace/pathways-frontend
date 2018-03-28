029d7fad0f64e0c483cd48de71f0900a3faf765e
rasmus <rasmus@peacegeeks.org>
2018-03-28 15:10:02 -0700

    Redux-persist works

--- src/hello_world/containers.tsx
+++ src/hello_world/containers.tsx
@@ -1,4 +1,4 @@
-import { Output, TwiceTheOutput, MyButton, Blink, Greeting } from './components';
+import { Output, TwiceTheOutput, MyButton, Greeting } from './components';
 import React from 'react';
 import { View, TextInput } from 'react-native';
 import * as counter from './counter';
@@ -17,7 +17,6 @@ export const MyContainer: React.SFC<Props> = (props) => {
     return (
         <View style={{ alignItems: 'center' }}>
             <Greeting name='Valeera' />
-            <Blink text='I love to blink' />
             <View style={{ flexDirection: 'row', padding: 20 }}>
                 <MyButton title='Increment' onPress={() => increment(value)} />
                 <MyButton title='Decrement' onPress={() => decrement(value)} />
