b287e3038f2fe0c81ce89d1e8e6984e6255e7ab2
rasmus <rasmus@peacegeeks.org>
2018-04-03 10:48:05 -0700

    Got a non-trivial test to run

--- src/hello_world/tests/components.test.tsx
+++ src/hello_world/tests/components.test.tsx
@@ -1,11 +1,31 @@
 import 'react-native';
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { Output } from '../components';
+import TestRenderer from 'react-test-renderer';
 
-it('output renders correctly', () => {
-    const tree = renderer.create(
-        <Output value={9} />
-    );
-    expect(tree).toBeDefined();
+it('a test', () => {
+
+    function MyComponent() {
+        return (
+            <div>
+                <SubComponent foo='bar' />
+                <p className='my'>Hello</p>
+            </div>
+        )
+    }
+
+    interface SubComponentProps {
+        foo: string
+    }
+
+    const SubComponent: React.SFC<SubComponentProps> = (props: SubComponentProps) => {
+        return (
+            <p className='sub'>{props.foo}</p>
+        );
+    }
+
+    const testRenderer = TestRenderer.create(<MyComponent />);
+    const testInstance = testRenderer.root;
+
+    expect(testInstance.findByType(SubComponent).props.foo).toBe('bar');
+    expect(testInstance.findByProps({ className: 'sub' }).children).toEqual(['bar']);
 });
