b5e2242d3941b71e7aa7e705c32c3fb4ea9454e6
rasmus <rasmus@peacegeeks.org>
2018-04-09 11:52:34 -0700

    Issue #26 Add type information to the redux connect call

--- src/hello_world/index.ts
+++ src/hello_world/index.ts
@@ -1,19 +1,19 @@
 import { connect } from 'react-redux';
-import { MyContainer } from './containers';
+import * as myContainer from './containers';
 import * as counter from '../stores/counter';
 import * as message from '../stores/message';
-import { Store, Dispatch } from '../application/store';
+import { Store } from '../application/store';
+import { Dispatch } from 'redux';
 
-/* tslint:disable:typedef */
-const mapStateToProps = (store: Store) => ({
+const mapStateToProps = (store: Store): myContainer.Props => ({
     counterInProps: store.counterInStore,
     messageInProps: store.messageInStore,
 });
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
+const mapDispatchToProps = (dispatch: Dispatch<Store>): myContainer.Actions => ({
     increment: (prop: counter.Store) => dispatch(counter.increment(prop)),
     decrement: (prop: counter.Store) => dispatch(counter.decrement(prop)),
     setMessage: (newMessage: string) => dispatch(message.setMessage(newMessage)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(MyContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(myContainer.MyContainer);
