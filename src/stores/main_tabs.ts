fe32bfd163b8bded5114c14532955e9208d98f20
rasmus <rasmus@peacegeeks.org>
2018-04-24 16:46:58 -0700

    Issue #34 Add validation to main tab reducer

--- src/stores/main_tabs.ts
+++ src/stores/main_tabs.ts
@@ -24,8 +24,27 @@ export const reducer = (store: Store = buildDefaultStore(), action?: SetMainTabA
     }
     switch (action.type) {
         case constants.SET_MAIN_TAB:
-            return { ...store, mainTab: action.payload.mainTab };
+            return { ...store, mainTab: validateMainPageId(action.payload.mainTab) };
         default:
             return store;
     }
+};
+
+// Using number as a type alias for MainPage here,
+// https://stackoverflow.com/questions/29706609/typescript-how-to-add-type-guards-for-enums-in-union-types/29706830#29706830
+
+const validateMainPageId = (pageId: number | string): MainPage => {
+    if (typeof pageId === 'string') {
+        return convertStringToTab(pageId);
+    }
+    return pageId;
+};
+
+const convertStringToTab = (pageId: string): MainPage => {
+    switch (pageId) {
+        case 'MainPage.One': return MainPage.One;
+        case 'MainPage.Two': return MainPage.Two;
+        case 'MainPage.Three': return MainPage.Three;
+    }
+    throw new Error(pageId + ': Invalid main page id, accepted values are MainPage.One, MainPage.Two or MainPage.Three');
 };
