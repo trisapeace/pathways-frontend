960e1d6b3ed49a24851c30c465bffbc8408197e9
rasmus <rasmus@peacegeeks.org>
2018-04-27 13:41:23 -0700

    Issue #41 Remove remains of tutorial code
    # Conflicts:
    #	src/components/hello_world/__tests__/components.test.tsx
    #	src/components/hello_world/__tests__/snapshots.test.tsx
    #	src/components/hello_world/components.tsx
    #	src/components/hello_world/connected_hello_world.ts
    #	src/components/hello_world/hello_world.tsx
    #	src/stores/__tests__/counter.test.ts
    #	src/stores/index.ts
    #	src/stores/message.ts

--- src/components/hello_world/__tests__/snapshots.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { shallow } from 'enzyme';
-
-it('should render correctly', () => {
-    const tree = shallow(<Text>This is a test!</Text>);
-    expect(tree).toMatchSnapshot();
-});
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,19 +1,13 @@
 import { combineReducers } from 'redux';
 import * as pageSwitcher from './page_switcher';
-import * as counter from './counter';
 import * as locale from './locale';
-import * as message from './message';
 
 export interface Store {
     readonly mainPageInStore: pageSwitcher.Store;
-    readonly counterInStore: counter.Store;
     readonly localeInStore: locale.Store;
-    readonly messageInStore: message.Store;
 }
 
 export const reducer = combineReducers<Store>({
     mainPageInStore: pageSwitcher.reducer,
-    counterInStore: counter.reducer,
     localeInStore: locale.reducer,
-    messageInStore: message.reducer,
 });
