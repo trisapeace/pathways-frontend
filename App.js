6769c16caaa60337b0d6a0e931ab933d2e15c603
Tom Nightingale <tom@tnightingale.com>
2018-05-01 13:58:35 -0700

    Issue #31 Add Intl polyfill for Android platform support

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
+// Unfortunately we need to load the Intl polyfill for Android. iOS has Intl
+// available natively; we should see it is possibly conditionally load the
+// polyfill only when needed.
+import 'intl';
+// Include Unicode CLDR locale data for each of our locales
+import 'intl/locale-data/jsonp/en.js';
+import 'intl/locale-data/jsonp/ar.js';
+
 import { Application } from './lib/application';
 
 export default Application;
