5defef0d101160b2d772b70e5ea408b2b85a5038
Ben St John <ben@affinitybridge.com>
2018-05-14 15:26:17 -0700

    Issue #44 Revert changes made to index.tsx to match master

--- src/application/index.tsx
+++ src/application/index.tsx
@@ -3,39 +3,14 @@ import { Provider } from 'react-redux';
 import * as main from '../components/main';
 import { store } from './store';
 import { ErrorBoundary } from './error_boundary';
-import * as taskList from '../components/task_list/task_list';
-
-const tasks = [
-    {
-        id: 1,
-        completed: false,
-        starred: false,
-        taskDefinition: {
-            id: 1,
-            category: 'Education',
-            description: 'Enroll child in elementary school.',
-            labels: ['Important', 'Do Soon', 'Free'],
-        },
-    },
-    {
-        id: 2,
-        completed: false,
-        starred: true,
-        taskDefinition: {
-            id: 2,
-            category: 'Education',
-            description: 'Enroll child in middle or secondary school.',
-            labels: ['Important', 'Free'],
-        },
-    },
-];
+import { ConnectedI18nProvider } from '../components/language_switcher/connected_i18n_provider';
 
 export const Application = (): JSX.Element => (
     <ErrorBoundary>
         <Provider store={store}>
-            <ConnectedNavigationBar>
-                <ConnectedHelloWorld />
-            </ConnectedNavigationBar>
+            <ConnectedI18nProvider>
+                <main.ConnectedComponent />
+            </ConnectedI18nProvider>
         </Provider>
     </ErrorBoundary>
 );
