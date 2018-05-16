9d43259ccefc2e1f587e4494140d5d442318b8e1
Theresa Deering <trisapeace@gmail.com>
2018-05-16 16:34:47 -0700

    Issue #74 Add initial page to route history
    - When creating the history in router.tx, now supplies the route to the
    Questionnaire page in the initialEntries so that when you push the Back
    button it will find the Questionnaire page in the history
    - Made a constant for the initial page and set it to the Questionnaire
    page so that I could use the constant both in router.tx's history and in
    page_switcher.ts's buildDefaultStore. Not sure if this is the best place
    to put that constant though or if there is a better way to get at that
    default store value from inside router.tx
    - Changed the route to start with /page instead of /user because it's
    more relevant
    - Changed the constant name to SET_MAIN_PAGE instead of SET_MAIN_TAB
    because everywhere else in the code seems to be calling them pages
    instead of tabs

--- src/application/router.ts
+++ src/application/router.ts
@@ -3,13 +3,16 @@ import * as reduxFirstRouter from 'redux-first-router';
 import createMemoryHistory from 'history/createMemoryHistory';
 import * as constants from '../application/constants';
 import { Store as StoreForApplicationState } from '../stores';
+import { initialPage } from '../stores/page_switcher';
 
 const routesMap = {
-    [constants.SET_MAIN_TAB]: '/user/:mainPage',
+    [constants.SET_MAIN_PAGE]: '/page/:mainPage',
 };
 
 export const createStoreWithRouter = (reducerForApplicationState: Reducer<StoreForApplicationState>) => {
-    const history = createMemoryHistory();
+    const history = createMemoryHistory({
+        initialEntries: [routesMap[constants.SET_MAIN_PAGE].replace(':mainPage', String(initialPage))],
+    });
     const router = reduxFirstRouter.connectRoutes(history, routesMap);
     const rootReducer = combineReducers({ location: router.reducer, applicationState: reducerForApplicationState });
     return createStore(rootReducer, compose(router.enhancer, applyMiddleware(router.middleware)));
--- src/stores/page_switcher.ts
+++ src/stores/page_switcher.ts
@@ -5,18 +5,20 @@ export enum Page {
     Questionnaire, MyPlan, ExploreAll,
 }
 
+export const initialPage = Page.Questionnaire;
+
 export type Store = Readonly<ReturnType<typeof buildDefaultStore>>;
 
 export type SetMainPageAction = Readonly<ReturnType<typeof setMainPage>>;
 
 // tslint:disable-next-line:typedef
 export const setMainPage = (mainPage: Page | string) => (
-    helpers.makeAction(constants.SET_MAIN_TAB, { mainPage })
+    helpers.makeAction(constants.SET_MAIN_PAGE, { mainPage })
 );
 
 // tslint:disable-next-line:typedef
 const buildDefaultStore = () => (
-    { mainPage: Page.Questionnaire }
+    { mainPage: initialPage }
 );
 
 export const reducer = (store: Store = buildDefaultStore(), action?: SetMainPageAction): Store => {
@@ -24,14 +26,14 @@ export const reducer = (store: Store = buildDefaultStore(), action?: SetMainPage
         return store;
     }
     switch (action.type) {
-        case constants.SET_MAIN_TAB:
+        case constants.SET_MAIN_PAGE:
             return { ...store, mainPage: validateMainPageId(action.payload.mainPage) };
         default:
             return store;
     }
 };
 
-// Using number as a type alias for the MainPage enum, see
+// Using number as a type alias for the Page enum, see
 // https://stackoverflow.com/questions/29706609/typescript-how-to-add-type-guards-for-enums-in-union-types/29706830#29706830
 
 const validateMainPageId = (pageId: number | string): Page => (
