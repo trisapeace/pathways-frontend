7d9a306b11a9da2d8ef575b2971dfee65cb150f0
Ben St John <ben@affinitybridge.com>
2018-05-17 13:28:39 -0700

    Issue #42 Stub in a bit of rough styling around a user's task list

--- src/components/my_plan/my_plan.tsx
+++ src/components/my_plan/my_plan.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Content, Text } from 'native-base';
+import { Content, Text, Container } from 'native-base';
 import * as tasks from '../tasks/index';
 
 export const Component: React.StatelessComponent = (): JSX.Element => (
-    <Content>
-        <Text>My plan</Text>
-        <tasks.ConnectedComponent />
-    </Content>
+    <Container>
+        <Content>
+            <Text>Saved Tasks</Text>
+            <tasks.ConnectedComponent />
+            <Text>Suggested Tasks</Text>
+            <tasks.ConnectedComponent />
+        </Content>
+    </Container>
 );
--- src/components/tasks/task.tsx
+++ src/components/tasks/task.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View } from 'react-native';
+import { Text, Button, Icon } from 'native-base';
 import { taskDetail as styles } from './styles';
-import { Tag } from './tag';
 import { TaskViewModel } from '../../selectors/tasks';
 import { TasksActions } from './actions';
-
-const getColorForTag = (label: string): string => {
-    switch (label.toLowerCase()) {
-        case 'important':
-            return '#f5a623';
-        case 'do soon':
-            return '#bd10e0';
-        case 'free':
-            return '#7dd322';
-        default:
-            return '#03a9f4';
-    }
-};
 
 export interface Props {
 }
@@ -24,26 +11,29 @@ export interface Props {
 export type Actions = TasksActions;
 
 export const Task: React.StatelessComponent<TaskViewModel & Actions> = (props: TaskViewModel & Actions): JSX.Element => (
-    <TouchableOpacity>
-        <View style={styles.wrapper}>
-            <View style={styles.sideColumn}>
-                <Text>{props.starred ? 'Starred' : 'Not starred'}</Text>
-            </View>
-            <View style={styles.centerColumn}>
-                <View style={styles.stackedItems}>
-                    <Text>{props.title}</Text>
-                    <View style={styles.inlineItems}>
-                        {props.tags.map((label: string, index: number) =>
-                         <Tag text={label} color={getColorForTag(label)} key={index} />)}
-                    </View>
+    <View style={styles.wrapper}>
+        <View style={styles.sideColumn}>
+            <Button rounded success>
+                <Text>Add</Text>
+            </Button>
+        </View>
+        <View style={styles.centerColumn}>
+            <View style={styles.stackedItems}>
+                <Text>{props.title}</Text>
+                {/*
+                <View style={styles.inlineItems}>
+                    {props.tags.map((label: string, index: number) =>
+                        <Tag text={label} color={getColorForTag(label)} key={index} />)}
                 </View>
-            </View>
-            <View style={styles.sideColumn}>
-                <View style={styles.stackedItems}>
-                    <Text>{'...'}</Text>
-                    <Text>{props.category}</Text>
-                </View>
+                */}
             </View>
         </View>
-    </TouchableOpacity>
+        <View style={styles.sideColumn}>
+            <View style={styles.stackedItems}>
+                <Button>
+                    <Icon name='arrow-forward' />
+                </Button>
+            </View>
+        </View>
+    </View>
 );
