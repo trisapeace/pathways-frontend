76e75b860015036d3d8a8f04f1190fe05f4b5388
Ben St John <ben@affinitybridge.com>
2018-05-22 12:56:59 -0700

    Issue #42 Remove Trans for dynamic content

--- src/components/tasks/task.tsx
+++ src/components/tasks/task.tsx
@@ -4,7 +4,6 @@ import { Text, Button, Icon } from 'native-base';
 import { taskDetail as styles } from './styles';
 import { TaskViewModel, taskViewToTask } from '../../selectors/tasks';
 import { TaskActions } from './actions';
-import { Trans } from '@lingui/react';
 import * as models from '../../stores/tasks';
 
 export interface Props {
@@ -18,13 +17,13 @@ export const Task: React.StatelessComponent<TaskViewModel & Actions> = (props: T
             <View style={styles.sideColumn}>
                 {!props.addToTaskList ? undefined : (
                     <Button rounded success onPress={(): models.AddToTaskListAction => props.addToTaskList(taskViewToTask(props))}>
-                        <Text><Trans>Add</Trans></Text>
+                        <Text>Add</Text>
                     </Button>
                 )}
             </View>
             <View style={styles.centerColumn}>
                 <View style={styles.stackedItems}>
-                    <Text><Trans>{props.title}</Trans></Text>
+                    <Text>{props.title}</Text>
                 </View>
             </View>
             <View style={styles.sideColumn}>
