96cc605780e03e0a232c58f9214302c7d7ffc985
Tom Nightingale <tom@tnightingale.com>
2018-05-30 16:00:55 -0700

    Issue #66 Fix broken logic

--- src/selectors/application_loading.ts
+++ src/selectors/application_loading.ts
@@ -2,6 +2,6 @@ import {} from '../application/store';
 import * as app from '../application/store';
 
 export const isApplicationLoading = (appStore: app.Store): boolean => {
-    return appStore.applicationState.fontsInStore.loading &&
+    return appStore.applicationState.fontsInStore.loading ||
         appStore.applicationState.localeInStore.loading;
 };
