50897e5696a2189afdae92d951a9de2b635d139e
rasmus <rasmus@peacegeeks.org>
2018-06-02 09:39:37 -0700

    Issue #66 Implement higher order component using pure function

--- src/components/main/loader.tsx
+++ src/components/main/loader.tsx
@@ -5,20 +5,9 @@ export interface LoaderProps {
     readonly loading: boolean;
 }
 
-export const withLoader = <ChildProps extends object>(ChildComponent: React.ComponentType<ChildProps>):
-    React.ComponentType<ChildProps & LoaderProps> => (
-        class LoaderComponent extends React.Component<ChildProps & LoaderProps> {
-            render() {
-                const { loading, ...props } = this.props as LoaderProps;
-                return loading ? this.renderLoading() : this.renderLoaded(props as ChildProps);
-            }
-
-            renderLoading(): JSX.Element {
-                return <Text>Loading...</Text>;
-            }
-
-            renderLoaded(props: ChildProps): JSX.Element {
-                return <ChildComponent {...props} />;
-            }
-        }
-    );
+export const withLoader =
+    <ChildProps extends object>
+        (ChildComponent: React.ComponentType<ChildProps>): React.SFC<ChildProps & LoaderProps> =>
+        ({ loading, ...props }: LoaderProps) => (
+            loading ? <Text>Loading...</Text> : <ChildComponent {...props} />
+        );
