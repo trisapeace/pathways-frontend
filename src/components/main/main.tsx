cc87a3b8641312f48267121e45e01e07cd143e55
Theresa Deering <trisapeace@gmail.com>
2018-06-06 10:07:05 -0700

    Issue #74 Revert mainPage/currentPage name change in one place
    - In this case, it seems like the name is coming from the file name instead of from the property in the store. So I think it makes more sense to keep it as mainPageSwitcher here.

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Container } from 'native-base';
 import * as header from './header';
-import * as currentPageSwitcher from './main_page_switcher';
+import * as mainPageSwitcher from './main_page_switcher';
 import * as footer from './footer';
 
-export type Props = header.Props & currentPageSwitcher.Props & footer.Props;
-export type Actions = header.Actions & currentPageSwitcher.Actions & footer.Actions;
+export type Props = header.Props & mainPageSwitcher.Props & footer.Props;
+export type Actions = header.Actions & mainPageSwitcher.Actions & footer.Actions;
 
 export const Component: React.StatelessComponent<Props & Actions> = (props: Props & Actions): JSX.Element => (
     <Container>
         <header.Component {...props} />
-        <currentPageSwitcher.Component {...props} />
+        <mainPageSwitcher.Component {...props} />
         <footer.Component {...props} />
     </Container >
 );
